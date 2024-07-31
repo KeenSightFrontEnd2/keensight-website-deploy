13556d62dd7d2436c2869068bad4297617d8d21c
Awii21 <awaissmr@gmail.com>
2024-07-31 06:39:07 +0500

    contact page string error fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,8 +46,10 @@ export default function About() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="message"
               placeholder="Your message"
-              rows="5"
-            ></textarea>
+              rows={5}
+            >
+
+            </textarea>
           </div>
           <div className="flex items-center justify-between">
             <button
