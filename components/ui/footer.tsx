91ced5298373cbad27175fc5c09b38efdc3a2ee4
Awii21 <awaissmr@gmail.com>
2024-08-04 15:51:45 +0500

    footer responsive issues fixed

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,11 +3,11 @@ import Logo from "./logo";
 export default function Footer() {
   return (
     <footer>
-      <div className="flex flex-col justify-center max-w-6xl mx-auto px-4 sm:px-6 items-center text-center lg:text-left">
+      <div className="flex flex-col justify-center max-w-6xl mx-auto px-4 sm:px-6 items-center text-center md:text-left lg:text-left">
         {/* Top area: Blocks */}
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
           {/* 1st block */}
-          <div className="sm:col-span-12 lg:col-span-3">
+          <div className="flex sm:col-span-12 lg:col-span-3 md:justify-center">
             <div className="mb-2 transition-all duration-500 hover:drop-shadow-2xl hover:-translate-x-1 hover:-translate-y-1">
               <Logo />
             </div>
